f2f8a537c4011381c27cab3fd616d8a5e5caabf6
David Leach <davidleach724@gmail.com>
2021-05-19 18:49:34 -0600

    Complete first half of medusa

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,29 +2,28 @@ class Magician {
   constructor(name, assistant, clothing) {
     this.name = `The Great ${name.name}`;
     this.assistant = name.assistant;
-    this.favoriteAccessory = name.clothing || 'top hat';
+    this.favoriteAccessory = name.clothing || "top hat";
     this.confidencePercentage = 10;
   }
 
   performIncantation(spell) {
-    return spell.toUpperCase() + '!';
+    return spell.toUpperCase() + "!";
   }
 
   performTrick() {
     this.confidencePercentage += 10;
-    if (this.favoriteAccessory === 'top hat') {
+    if (this.favoriteAccessory === "top hat") {
       return `PULL RABBIT FROM TOP HAT`;
     }
     return `PULL DOVE FROM SLEEVE`;
   }
 
   performShowStopper() {
-    if(this.confidencePercentage >= 100 && this.assistant === true) {
+    if (this.confidencePercentage >= 100 && this.assistant === true) {
       return `WOW! The magician totally just sawed that person in half!`;
     }
-      return `Oh no, this trick is not ready!`;
+    return `Oh no, this trick is not ready!`;
   }
 }
-
 
 module.exports = Magician;
--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,17 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim() {
+    this.statues.push();
+    console.log();
+  }
+}
+
+
+module.exports = Medusa;
