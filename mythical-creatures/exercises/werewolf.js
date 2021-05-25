7510042199e6e12e20c6faba5e05ecd5b087205d
David Leach <davidleach724@gmail.com>
2021-05-24 19:38:40 -0600

    Complete werewolf

---
+++ mythical-creatures/exercises/werewolf.js
@@ -0,0 +1,32 @@
+var Victim = require("./victim");
+
+class Werewolf {
+  constructor(name) {
+    this.name = name;
+    this.form = `human`;
+    this.hungry = false;
+  }
+
+  completeTransformation() {
+    if (this.form === `human`) {
+      this.form = `wolf`;
+      this.hungry = true;
+      return `Aaa-Woooo!`;
+    } else {
+      this.form = `human`;
+      this.hungry = false;
+      return `Where are I?`;
+    }
+  }
+
+  eatVictim(victim) {
+    if(this.form === `human`) {
+      return `No way am I eating Baby, I'd like a burger!`;
+    }
+    victim.alive = false;
+    this.form = `human`;
+    return `Yum, ${victim.name} was delicious.`
+  }
+}
+
+module.exports = Werewolf;
