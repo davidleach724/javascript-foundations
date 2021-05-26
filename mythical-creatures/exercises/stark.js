2f7797015a7fa6fb576b0fd4a414c0dbd605fff5
David Leach <davidleach724@gmail.com>
2021-05-25 20:08:08 -0600

    Add direwolf progress

---
+++ mythical-creatures/exercises/stark.js
@@ -0,0 +1,9 @@
+class Stark {
+  constructor(stark) {
+    this.stark = {stark};
+    this.name = stark.name;
+    console.log(stark);
+    console.log(this.stark);
+    }
+  }
+module.exports = Stark;
