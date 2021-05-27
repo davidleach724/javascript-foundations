b583fba95ba99cbf156a908c376bee0bfd5faa04
David Leach <davidleach724@gmail.com>
2021-05-26 18:47:37 -0600

    Mostly finish direwolf

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -1,8 +1,26 @@
+var Stark = require('./stark');
+
 class Direwolf {
   constructor(name, home, size) {
     this.name = name;
     this.home = home || `Beyond the Wall`;
     this.size = size || 'Massive';
+    this.starksToProtect = [];
+    this.huntsWhiteWalkers = true;
+  }
+
+  protect(stark) {
+    if(this.home === stark.location &&
+       this.starksToProtect.length < 2) {
+         this.huntsWhiteWalkers = false;
+         stark.safe = true;
+         this.starksToProtect.push(stark);
+    }
+  }
+
+  leave (stark) {
+    this.starksToProtect = [];
+    stark.safe = false;
   }
 }
 
--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,9 +1,26 @@
+var Direwolf = require('./direwolf');
+
 class Stark {
   constructor(stark) {
-    this.stark = {stark};
+    if(!stark){return}
     this.name = stark.name;
-    console.log(stark);
-    console.log(this.stark);
+    this.location = stark.area || 'Winterfell';
+    this.safe = false;
+  }
+
+  sayHouseWords()  {
+    if(!this.safe) {
+      return `Winter is Coming`;
+    } else {
+      return `The North Remembers`;
     }
   }
+
+  callDirewolf(name, home) {
+    console.log(name, home);
+    var direwolf = new Direwolf(name, home);
+    // return direwolf;
+  }
+}
+
 module.exports = Stark;
