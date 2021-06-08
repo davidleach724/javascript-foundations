af69266ccab7ec05c75d888db1cb6bc5589d85de
David Leach <davidleach724@gmail.com>
2021-06-08 16:57:38 -0600

    Complete lunchbox

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,31 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = 'extra';
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    this.isHealthy = true;
+  }
 
+  getEaten() {
+    this.amount -= 10;
+    if(this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
+
+  checkForHealthy() {
+    var snackName = this.type.toLowerCase();
+
+    if(snackName.search('fruit') >= 0) {
+      this.isHealthy = true;
+      return true;
+    } else {
+      this.isHealthy = false;
+      return false;
+    }
+  }
 }
 
 module.exports = Snack;
---
+++ spaceport/src/part.js
@@ -0,0 +1,26 @@
+class Part {
+  constructor(part) {
+    this.name = part.name;
+    this.type = part.type;
+    this.value = part.value;
+    this.broken = false;
+    this.isValid = true;
+
+    if(this.name === undefined) {
+      this.checkForValidity = `This part needs a name!`
+      this.isValid = false;
+    }
+
+    if(this.type === undefined) {
+      this.checkForValidity = `This part needs a type!`
+      this.isValid = false;
+    }
+
+    if(this.value === undefined) {
+      this.checkForValidity = `This part needs a value!`
+      this.isValid = false;
+    }
+  }
+}
+
+module.exports = Part;
