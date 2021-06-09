049b7d44018f22ffa9adad064c51be307103a018
David Leach <davidleach724@gmail.com>
2021-06-09 08:05:53 -0600

    Complete roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,27 @@
 class Roadrace {
+  constructor(race) {
+    this.name = race.title;
+    this.location = race.city;
+    this.distance = 0;
+    this.participants = [];
+  }
 
+  setDistance(distance) {
+    this.distance = distance;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`;
+  }
+
+  registerParticipants(participant) {
+    this.participants.push(participant);
+  }
+
+  completeRace() {
+    //invoke the method runRace with raceName and raceDistance
+    //console.log(this.participants);
+    for (var i=0; i < this.participants.length; i++) {
+      this.participants[i].runRace(this.name, this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,25 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(miles) {
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
+
+  stretch() {
+    this.fitness += .5;
+  }
+
+  runRace(raceName, raceDistance) {
+    this.completedRaces.push(raceName);
+    this.runSomeMiles(raceDistance);
+  }
 }
 
 module.exports = Runner;
