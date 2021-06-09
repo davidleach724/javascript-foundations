11a0520b9e66129d222cca17079273a8e419b559
David Leach <davidleach724@gmail.com>
2021-06-08 22:15:15 -0600

    Complete vendingmachine

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,34 @@
 class VendingMachine {
+  constructor(vendingMachine) {
+    this.id = vendingMachine.id;
+    this.isBroken = vendingMachine.isBroken;
+    this.snacks = [];
+  }
 
+  addSnacks(snack) {
+    for (var i=0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name === snack.name) {
+        return `Sorry, that snack is already stocked! Try adding a different snack.`
+      }
+    }
+    this.snacks.push(snack);
+  }
+
+  purchaseSnack(snack, paid) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name === snack) {
+        if (this.snacks[i].itemsInStock === 0) {
+          return `Sorry, no items in stock. Try another item.`
+        }
+        if (this.snacks[i].price <= paid) {
+          this.snacks[i].itemsInStock--;
+          return `Success! Here is $${paid - this.snacks[i].price} back!`
+        } else {
+          return `Sorry, not enough payment. Please add more money.`
+        }
+      }
+    }
+  }
 }
 
 module.exports = VendingMachine;
